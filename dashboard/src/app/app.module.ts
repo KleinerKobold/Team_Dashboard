f7d795a34880820bc0ea3f2a59ffd6998ded9e34
Oliver Staudt <Oliver.Staudt@deutschebahn.com>
2021-04-27 12:28:03 +0200

    added team member component

---
+++ dashboard/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TeamMembersComponent } from './team-members/team-members.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeamMembersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
